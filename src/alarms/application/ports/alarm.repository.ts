9b80850cd1536ab9e6e4777a6edd6098e3d4ba74
a1740942002 <a1740942002@me.com>
2023-12-07 22:30:51 +0800

    Experimenting with CQRS. Part 1

--- src/alarms/application/ports/alarm.repository.ts
+++ src/alarms/application/ports/alarm.repository.ts
@@ -1,5 +1,7 @@
 import { Alarm } from '../../domain/alarm';
 
+// 等同於 TypeScript 的 interface
+// 但因為 runtime 時，JavaScript 沒有 interface 的概念，所以用 abstract class 來實現
 export abstract class AlarmRepository {
   abstract findAll(): Promise<Alarm[]>;
   abstract save(alarm: Alarm): Promise<Alarm>;
