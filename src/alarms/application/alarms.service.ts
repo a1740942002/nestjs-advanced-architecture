2802d2cbbaec6336364a4bac08305873fe4f31a9
a1740942002 <a1740942002@me.com>
2023-12-07 20:30:45 +0800

    Hexagonal Architecture in Practice. Part 1

---
+++ src/alarms/application/alarms.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAlarmDto } from '../presenters/http/dto/create-alarm.dto';
+import { UpdateAlarmDto } from '../presenters/http/dto/update-alarm.dto';
+
+@Injectable()
+export class AlarmsService {
+  create(createAlarmDto: CreateAlarmDto) {
+    return 'This action adds a new alarm';
+  }
+
+  findAll() {
+    return `This action returns all alarms`;
+  }
+}
